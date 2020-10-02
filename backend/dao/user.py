68b3073540c2862df0cc04a4a4fc90f9575c7948
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2020-10-02 09:51:35 -0400

    implemented naive register method

--- backend/dao/user.py
+++ backend/dao/user.py
@@ -1,8 +1,11 @@
-from dao.dummy_data import users_dummy_data
+from dao.dummy_data import users_dummy_data, uid_count
 
 class UserDAO:
     def get(self, username):
+        # TODO: Connect to database and make query 
         return [ user for user in users_dummy_data if user[1] == username ][0]
 
-    def add(self):
-        pass
+    def add(self, json):
+        # TODO: Connect to database and make query
+         newTeam = (uid_count, json['username'], json['email'])
+         return newTeam
--- backend/handler/user.py
+++ backend/handler/user.py
@@ -1,17 +1,26 @@
 from flask import jsonify
-from handler.utils import OK, BAD_REQUEST
+from handler.utils import OK, BAD_REQUEST, CREATED
 from passlib.hash import sha256_crypt
 from dao.user import UserDAO
 
 class User:
     def register(self, json):
+        if json['username'] and json['email'] and json['password']:
+            if not UserDAO.get(json['username']):
+                json['password'] = sha256_crypt.hash(json['password']) 
+                user = UserDAO.add(json)
+                return jsonify(isAuth = True, UserID = user[0], Username = user[1]), CREATED
+            else:
+                return jsonify(Error = 'Username already in use'), BAD_REQUEST
+        else:
+            return jsonify(Error = 'Unexpected attributes in post request'), BAD_REQUEST
         pass
 
     def login(self, json):
         if json['username'] and json['password']:
             user = UserDAO().get(json['username'])
             if sha256_crypt.verify(json['password'], user[3]):
-                return jsonify(IsAuth = True, UserID = user[0]), OK
+                return jsonify(IsAuth = True, UserID = user[0], Username = user[1]), OK
             else:
                 return jsonify(IsAuth = False), OK
         else:
