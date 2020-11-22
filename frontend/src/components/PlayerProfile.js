4eb80f61fffbecb09ff4e6d3617c924e77b30fd8
Miguel Vazquez <miguel.vazquez2@upr.edu>
2020-11-22 14:02:54 -0400

    added axios http request untested

--- frontend/src/components/PlayerProfile.js
+++ frontend/src/components/PlayerProfile.js
@@ -7,32 +7,58 @@ import { faBalanceScale } from '@fortawesome/free-solid-svg-icons';
 
 function PlayerProfile() {
     const [state, setState] = useContext(AuthContext);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [playerData, setPlayerData] = useState({});
 
-    return (
-        <Container>
-            <div>
-            <Row>
-                <Col>
-                    <h1>Mario Diaz</h1>
-                </Col>
-                <Col className="m-2 d-flex justify-content-end">
-                    <Button>Compare <FontAwesomeIcon icon={faBalanceScale}/></Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h4>Player Info goes here</h4>
-                </Col>
-            </Row>
-            </div>
-            <Nav justify variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Statistics</Nav.Link>
-                </Nav.Item>
-            </Nav>
-            <PlayerStatisticsContent state={state}/>
-        </Container>
-    );
+    useEffect(() => {
+        const fetchPlayerData = async () => {
+            setIsLoading(true)
+            await axios.get(`http://localhost:5000/player`)
+                .then((response) => {
+                    setPlayerData(response.data.Player[0]);
+                    setIsError(false);
+                })
+                .catch(() => setIsError(true));
+            setIsLoading(false)
+        }
+        fetchPlayerData();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <Container align="center">
+                <h1>Loading...</h1>
+                <Spinner animation="border"/>
+            </Container>
+        )
+    } else {
+        return (
+            <Container>
+                <div>
+                    <Row>
+                        <Col>
+                            <h1>Mario Diaz</h1>
+                        </Col>
+                        <Col className="m-2 d-flex justify-content-end">
+                            <Button>Compare <FontAwesomeIcon icon={faBalanceScale}/></Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h4>Player Info goes here</h4>
+                        </Col>
+                    </Row>
+                </div>
+                <Nav justify variant="tabs" defaultActiveKey="/home">
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">Statistics</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <PlayerStatisticsContent state={state}/>
+            </Container>
+        );
+    }
 }
 
 export default PlayerProfile
--- frontend/src/components/Teams.js
+++ frontend/src/components/Teams.js
@@ -4,21 +4,45 @@ import Searchbar from '../components/Searchbar';
 import TeamPreview from '../components/TeamPreview';
 
 function Teams() {
-    const dummyData = [
-        { teamName: 'Los Coquis', teamID: 2, sportName: 'soccer', teamMemberLength: 8},
-        { teamName: 'Bravos de Ponce', teamID: 3, sportName: 'soccer', teamMemberLength: 12},
-        { teamName: 'Cangrejeros', teamID: 4, sportName: 'soccer', teamMemberLength: 10},
-        { teamName: 'Gurabo FC', teamID: 6, sportName: 'soccer', teamMemberLength: 14},
-    ]
+    const [state, setState] = useContext(AuthContext);
+    const [show, setShow] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [teamsData, setTeamsData] = useState({});
+
+    useEffect(() => {
+        const fetchTeamData = async () => {
+            setIsLoading(true)
+            await axios.get(`http://localhost:5000/team`)
+                .then((response) => {
+                    setTeamsData(response.data.Teams);
+                    setIsError(false);
+                    })
+                .catch(() => setIsError(true));
+            setIsLoading(false)
+        }
+
+        fetchTeamData();
+    }, []);
+    var teamList = [];
+    for(i=0; i < teamsData.length; i++){
+      teamList.push({ teamName: teamsData.teamName, teamID: teamsData.teamID, sportName: teamsData.teamSports, teamMemberLength: teamsData.teamMemberLength});
+    }
+    // const dummyData = [
+    //     { teamName: 'Los Coquis', teamID: 2, sportName: 'soccer', teamMemberLength: 8},
+    //     { teamName: 'Bravos de Ponce', teamID: 3, sportName: 'soccer', teamMemberLength: 12},
+    //     { teamName: 'Cangrejeros', teamID: 4, sportName: 'soccer', teamMemberLength: 10},
+    //     { teamName: 'Gurabo FC', teamID: 6, sportName: 'soccer', teamMemberLength: 14},
+    // ]
 
     return (
         <div>
             <Container>
                 <Searchbar title="Teams" placeholder="Search Teams"/>
-                {dummyData.map(team => (
+                {teamList.map(team => (
                     <TeamPreview key={team.teamID} noLink={false} teamName={team.teamName} teamID={team.teamID} teamMemberLength={team.teamMemberLength}/>
                 ))}
-            </Container>   
+            </Container>
         </div>
     )
 }
