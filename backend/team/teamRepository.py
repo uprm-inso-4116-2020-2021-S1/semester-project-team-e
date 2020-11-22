e7b23188aa981de635a4d9202fddf4a85c74fba1
ismael <ismael.rios2@upr.edu>
2020-11-21 21:16:06 -0400

    add edit delete se refleja en la abse de datos

--- backend/team/teamRepository.py
+++ backend/team/teamRepository.py
@@ -38,29 +38,41 @@ class TeamRepository:
 
     def add(self, team):
         cursor = self.conn.cursor()
-        query = "INSERT INTO team(team_name, info) values(%s, %s) returning id;"
-        teamid = cursor.execute(query, [team.team_name], [team.team_info])
-        query2 = "SELECT id FROM sport WHERE sportname = %s;"
-        sportid = cursor.execute(query2, [team.sport_name])
-        query3 = "INSERT INTO team_sport(team_id, sport_id) values(%s, %s);"
-        cursor.execute(query3, [teamid], [sportid])
-        return teamid
+        query = "INSERT INTO team(team_name, info) values(?, ?)"
+        cursor.execute(query, (team.team_name, team.team_info,))
+        teamid = cursor.lastrowid
+        query2 = "SELECT id FROM sport WHERE sportname = ?"
+        cursor.execute(query2, (team.sport_name,))
+        sportid = cursor.fetchone()[0]
+        query3 = "INSERT INTO team_sport(team_id, sport_id) values(?, ?)"
+        cursor.execute(query3, (teamid, sportid,))
+        self.conn.commit()
+        team_obj = self.get(teamid)
+        return team_obj
 
     def edit(self, team):
         cursor = self.conn.cursor()
-        query = "UPDATE team SET team_name = %s, info = %s;"
-        cursor.execute(query, [team.team_name], [team.team_info])
-        query2 = "SELECT id FROM sport WHERE sportname = %s;"
-        sportid = cursor.execute(query2, [team.sport_name])
-        query3 = "UPDATE team_sport SET team_id = %s, sport_id = %s;"
-        cursor.execute(query3, [team.team_id], [sportid])
-        return
+        query = "UPDATE team SET team_name = ?, info = ? WHERE id = ?"
+        cursor.execute(query, (team.team_name, team.team_info, team.team_id,))
+        query2 = "SELECT id FROM sport WHERE sportname = ?"
+        cursor.execute(query2, (team.sport_name,))
+        sportid = cursor.fetchone()[0]
+        query3 = "UPDATE team_sport SET team_id = ?, sport_id = ? WHERE team_id = ?"
+        cursor.execute(query3, (team.team_id, sportid, team.team_id,))
+        self.conn.commit()
+        team_obj = self.get(team.team_id)
+        return team_obj
+
 
     def delete(self, tid):
+        team_obj = self.get(tid)
         cursor = self.conn.cursor()
-        query = "DELETE FROM team WHERE id = %s;"
-        cursor.execute(query, [tid])
-        return
+        query1 = "DELETE FROM team_sport WHERE team_id = ?"
+        cursor.execute(query1, (tid,))
+        query2 = "DELETE FROM team WHERE id = ?"
+        cursor.execute(query2, (tid,))
+        self.conn.commit()
+        return team_obj
 
     def getBySport(self, sport_name):
         cursor = self.conn.cursor()
@@ -89,7 +101,7 @@ class TeamRepository:
     def getByNameAndSport(self, team_name, sport_name):
         cursor = self.conn.cursor()
         query = "SELECT team.id, team_name, info, sportname FROM ((team JOIN team_sport ON team.id = team_id) JOIN sport ON sport_id = sport.id) WHERE team_name = %s AND sportname = %s;"
-        cursor.execute(query, [team_name], [sport_name])
+        cursor.execute(query, team_name, sport_name)
         team_tup = cursor.fetchall()
         team_obj = [Team(team[0], team[1], team[2], team[3]) for team in team_tup]
         for team in team_obj:
@@ -99,4 +111,4 @@ class TeamRepository:
         return team_obj
 
     def getAvgStats(self, tid):
-        return next(avg_stat[1:] for avg_stat in soccer_team_avg_dummy_data if avg_stat[0] == tid)
+        return next(avg_stat[1:] for avg_stat in SoccerTeamDAO.getByid(tid))
