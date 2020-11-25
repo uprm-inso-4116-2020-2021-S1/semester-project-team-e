f0b04edd200f9647a67938d72e8ac2031cece43d
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2020-11-24 22:31:51 -0400

    implemented add team and getMyTeams on myTeams page

--- frontend/src/components/MyTeams.js
+++ frontend/src/components/MyTeams.js
@@ -1,36 +1,68 @@
-import React, {useState, useContext, useEffect, useRef} from 'react';
+import React, {useState, useContext, useEffect, useRef, useCallback} from 'react';
 import {Redirect} from 'react-router-dom';
-import {Container, Col, Row, Button, Modal, Form} from 'react-bootstrap';
+import {Container, Col, Row, Button, Modal, Form, Spinner} from 'react-bootstrap';
 import Searchbar from '../components/Searchbar';
 import TeamPreview from '../components/TeamPreview';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPlus, faTrash} from '@fortawesome/free-solid-svg-icons';
+import {faPlus, faTrash, faFrown} from '@fortawesome/free-solid-svg-icons';
 import {AuthContext} from './AuthContext';
 import {useForm, Controller} from 'react-hook-form';
 import axios from 'axios';
 
+
 function MyTeams() {
     const [state] = useContext(AuthContext);
     const [show, setShow] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [myTeams, setMyTeams] = useState([]);
     const [newTeam, setNewTeam] = useState({});
+    // const [teamIdToDelete, setTeamIdToDelete] = useState(70);
     const isInitialMount = useRef(true);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     useEffect(() => {
+        const getMyTeams = async () => {
+            setIsLoading(true);
+            await axios.post('http://localhost:5000/manager/myteams', {username: state.name})
+                .then((response) => {
+                    setMyTeams(response.data.Teams);
+                    setIsError(false);
+                })
+                .catch(() => setIsError(true));
+            setIsLoading(false);
+        }
+
         const addTeam = async () => {
             await axios.post('http://localhost:5000/team', newTeam)
-                .then((reponse) => {})
+                .then((response) => {})
                 .catch((error) => {})
         }
 
         if (isInitialMount.current) {
             isInitialMount.current = false;
+            getMyTeams();
         } else {
             addTeam();
+            getMyTeams();
         }
-    }, [newTeam])
+    }, [newTeam]);
+
+    // useEffect(() => {
+        // const deleteTeam = async () => {
+            // await axios.delete(`http://localhost:5000/team/${teamIdToDelete}`)
+                // .then((response) => {})
+                // .catch(() => setIsError(true));
+        // }
+
+        // if (isInitialMount.current) {
+            // isInitialMount.current = false;
+        // } else {
+            // deleteTeam();
+        // }
+    // }, [teamIdToDelete])
 
     const dummyData = [
         { teamName: 'Los Coquis', teamID: 2, sportName: 'soccer', teamMemberLength: 8},
@@ -42,6 +74,7 @@ function MyTeams() {
 
         const onSubmit = data => {
             data.username = state.name;
+            setNewTeam(data)
             console.log(data);
             handleClose();
         }
@@ -89,6 +122,49 @@ function MyTeams() {
         )
     }
 
+    const TeamList = () => {
+        if (isLoading) {
+            return (
+                <Container align="center">
+                    <h1>Loading...</h1>
+                    <Spinner animation="border"/>
+                </Container>
+            )
+        } else {
+            if (isError) {
+                return (
+                    <Container>
+                        <Col align="center" className="my-3">
+                            <FontAwesomeIcon icon={faFrown} size="9x"/>
+                            <h1>Something went wrong...</h1>
+                        </Col>
+                    </Container>
+                )
+            } else {
+                if (myTeams.length === 0) {
+                    return (
+                        <Container align="center" className="m-2">
+                            <h1>No teams found...</h1>
+                        </Container>
+                    )
+                } else {
+                    return (
+                        myTeams.map(team => (
+                            <Row key={team.team}>
+                                <Col>
+                                    <TeamPreview teamName={team.team_name} teamID={team.team} teamMemberLength={0}/>
+                                </Col>                        
+                                <Col xs="auto" className="align-self-center">
+                                    <Button variant="light"><FontAwesomeIcon icon={faTrash}/></Button>
+                                </Col>
+                            </Row>
+                        ))
+                    )
+                }
+            }
+        }
+    }
+
     if (state.name) {
         return (
             <div>
@@ -102,16 +178,7 @@ function MyTeams() {
                             <Button onClick={handleShow}>Add Team <FontAwesomeIcon icon={faPlus}/></Button>
                         </Col>
                     </Row>
-                    {dummyData.map(team => (
-                        <Row key={team.teamID}>
-                            <Col>
-                                <TeamPreview  teamName={team.teamName} teamID={team.teamID} teamMemberLength={team.teamMemberLength}/>
-                            </Col>                        
-                            <Col xs="auto" className="align-self-center">
-                                <Button variant="light"><FontAwesomeIcon icon={faTrash}/></Button>
-                            </Col>
-                        </Row>
-                    ))}
+                    <TeamList myTeams={myTeams}/> 
                 </Container>    
             </div>
         )
