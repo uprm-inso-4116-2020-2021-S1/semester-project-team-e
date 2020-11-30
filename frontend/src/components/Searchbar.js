37ab3e4b8a9532985fafba26fece18b867d73928
Miguel44 <miguel.vazquez2@upr.ed>
2020-11-29 23:09:09 -0400

    added team search results

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
-import {Form, Row, Col, Button} from 'react-bootstrap';
+import {Form, Row, Col, Button, Modal, Container} from 'react-bootstrap';
 import {useForm} from 'react-hook-form';
+import {Redirect} from 'react-router-dom';
+import axios from "axios";
+import TeamPreview from "./TeamPreview";
 
 function Searchbar(props) {
-     const { register, handleSubmit, watch, errors } = useForm();
-     const onSubmit = data => console.log(data);
+     const { register, handleSubmit} = useForm();
+     const [teamsData, setTeamsData] = useState([]);
+     const [show, setShow] = useState(false);
+     const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+     const onSubmit = async data => {
+            await axios.get(`http://localhost:5000/team?keyword=${data.search}`)
+            .then((response) => {
+            if(response.data.Teams){
+                console.log(response.data);
+                setTeamsData(response.data.Teams)
+                handleShow();
+            }
+                })
+     }
+     if(show){
+         return(
+             <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            <Modal.Title>Results</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div>
+                    <Container>
+                        {teamsData.map(team => (
+                            <TeamPreview key={team.team} noLink={false} teamName={team.team_name}
+                                         teamID={team.team} teamMemberLength={0}/>
+                        ))}
+                    </Container>
+                </div>
+            </Modal.Body>
+        </Modal>
+         )}
 
     return (
         <div>
